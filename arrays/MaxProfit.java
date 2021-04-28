f5d6c46890c95434e3558d7870014f84d6513b02
tarunraghav226 <tarun.raghav226@gmail.com>
2021-04-28 19:02:03 +0530

    Added solution for max profit after selling stock problem.

---
+++ arrays/MaxProfit.java
@@ -0,0 +1,19 @@
+public class MaxProfit {
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i=0;i<prices.length;i++){
+            if(minPrice > prices[i]){
+                minPrice = prices[i];
+            }
+            else if(prices[i]-minPrice > maxProfit){
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int[] arr = {7,1,5,3,6,4};
+        System.out.println(maxProfit(arr));
+    }
+}
