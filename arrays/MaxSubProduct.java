bc9f5f3006962c3a43def1b8bdfb134cee3d4c0d
tarunraghav226 <tarun.raghav226@gmail.com>
2021-05-04 20:23:21 +0530

    Added solution.

---
+++ arrays/MaxSubProduct.java
@@ -0,0 +1,39 @@
+public class MaxSubProduct {
+    static long maxProduct(int[] arr, int n) {
+        // code here
+        long minVal = arr[0];
+        long maxVal = arr[0];
+    
+        long maxProduct = arr[0];
+    
+        for (int i = 1; i < n; i++)
+        {
+    
+            // When multiplied by -ve number,
+            // maxVal becomes minVal
+            // and minVal becomes maxVal.
+            if (arr[i] < 0)
+            {
+                long temp = maxVal;
+                maxVal = minVal;
+                minVal =temp;
+            
+            }
+    
+            // maxVal and minVal stores the
+            // product of subarray ending at arr[i].
+            maxVal = Math.max(arr[i], maxVal * arr[i]);
+            minVal = Math.min(arr[i], minVal * arr[i]);
+    
+            // Max Product of array.
+            maxProduct = Math.max(maxProduct, maxVal);
+        }
+    
+        // Return maximum product found in array.
+        return maxProduct;
+    }
+    public static void main(String[] args) {
+        int[] arr = {6,-3,-10,0,2};
+        System.out.println(maxProduct(arr, 5));
+    }
+}
