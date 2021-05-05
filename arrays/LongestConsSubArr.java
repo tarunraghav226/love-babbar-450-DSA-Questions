63c049037665edf4ca0dc7916898e08da48c08ac
tarunraghav226 <tarun.raghav226@gmail.com>
2021-05-05 19:45:30 +0530

    Updated solution.

--- arrays/LongestConsSubArr.java
+++ arrays/LongestConsSubArr.java
@@ -1,22 +1,29 @@
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class LongestConsSubArr{
     static int findLongestConseqSubseq(int arr[], int N){
 	   // add your code here
-       int globalMax =0, currMax = 1;
-       Arrays.sort(arr);
-        for(int i=0;i<N-1;i++){
-            if(arr[i]+1 == arr[i+1]){
-                currMax += 1;
-            }
-            else{
-                if(globalMax < currMax){
-                    globalMax = currMax;
-                }
-                currMax = 1;
-            }
-        }
-       return globalMax;
+       HashSet<Integer> set = new HashSet<>();
+       for(int val: arr){
+           set.add(val);
+       }
+
+       int longestSeq = 1;
+       for(int val: arr){
+           if(!set.contains(val-1)){
+               int curNum = val;
+               int curSeq = 1;
+
+               while(set.contains(curNum+1)){
+                   curNum += 1;
+                   curSeq += 1;
+               }
+
+               longestSeq = Math.max(longestSeq, curSeq);
+           }
+       }
+       return longestSeq;
 	}
     public static void main(String[] args) {
         int[] arr = {2,6,1,9,4,5,3};
