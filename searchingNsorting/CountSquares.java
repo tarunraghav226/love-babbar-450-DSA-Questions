e028c05ade1b4a802d4049245b15f1e3854e343c
tarunraghav226 <tarun.raghav226@gmail.com>
2021-06-04 00:01:10 +0530

    Updated sol.

--- searchingNsorting/CountSquares.java
+++ searchingNsorting/CountSquares.java
@@ -1,12 +1,17 @@
 public class CountSquares {
     static int countSquares(int N) {
+        if(N ==0) return 0;
         int l = 1;
         int r = N;
+        int ans = 1;
         while(l<=r){
             int mid = (l+r)/2;
-            if(Math.pow(mid,mid) < N) return mid;
-            else if(Math.pow(mid,mid) >= N) r = mid-1; 
+            if(mid*mid < N) {
+                ans=mid;
+                l = mid+1;
+            }
+            else if(mid*mid >= N) r = mid-1; 
         }
-        return -1;
+        return ans;
     }
 }
