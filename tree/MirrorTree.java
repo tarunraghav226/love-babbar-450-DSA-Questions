443703f985f6ebd0ae6615bd683ee09c7bb2371a
tarunraghav226 <tarun.raghav226@gmail.com>
2021-07-06 14:28:54 +0530

    Added sol.

---
+++ tree/MirrorTree.java
@@ -0,0 +1,13 @@
+package tree;
+
+public class MirrorTree {
+    void mirror(Node node) {
+        if(node == null) return ;
+
+        Node temp = node.left;
+        node.left=node.right;
+        node.right=temp;
+        mirror(node.left);
+        mirror(node.right);
+    }
+}
