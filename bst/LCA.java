2bdc40b3fa96345549d61008b5b9469abaa10fdd
tarunraghav226 <tarun.raghav226@gmail.com>
2021-07-14 14:57:09 +0530

    Added sol.

---
+++ bst/LCA.java
@@ -0,0 +1,11 @@
+package bst;
+
+public class LCA {
+    Node LCA(Node root, int n1, int n2){
+        // code here.    
+        if(root == null) return null;
+        if(n1<root.data && n2<root.data) return LCA(root.left, n1, n2);
+        if(n1>root.data && n2>root.data) return LCA(root.right, n1, n2);
+        return root;
+    }
+}
