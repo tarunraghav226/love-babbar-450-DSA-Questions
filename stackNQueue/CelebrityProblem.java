f01874dfd6738db60a78c1a55463c42c1b7250d7
tarunraghav226 <tarun.raghav226@gmail.com>
2021-08-11 14:14:30 +0530

    Added sol.

---
+++ stackNQueue/CelebrityProblem.java
@@ -0,0 +1,13 @@
+public class CelebrityProblem {
+    int celebrity(int M[][], int n){
+    	// code here 
+    	int c=0;
+    	for(int i=1;i<n;i++){
+    	    if(M[c][i]==1) c = i;
+    	}
+    	for(int i=0;i<n;i++){
+    	    if(i!=c && (M[c][i]==1 || M[i][c]==0))return -1;
+    	}
+    	return c;
+    }
+}
